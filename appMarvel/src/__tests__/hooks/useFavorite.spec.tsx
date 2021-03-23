9272d2455c1bddff7b9283e0bc49f3c29c6c6cc4
Leonardo Leite <leonardotleite13k@gmail.com>
2021-03-22 23:44:32 -0300

    Finished tests

--- appMarvel/src/__tests__/hooks/useFavorite.spec.tsx
+++ appMarvel/src/__tests__/hooks/useFavorite.spec.tsx
@@ -1,3 +1,4 @@
+import { Alert } from 'react-native';
 import { renderHook, act } from '@testing-library/react-hooks';
 import MockAdapater from 'axios-mock-adapter';
 import AsyncStorage from '@react-native-async-storage/async-storage';
@@ -5,6 +6,156 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { FavoriteProvider, useFavorite } from '../../hooks/favorite';
 import { ComicProvider, useComic } from '../../hooks/comic';
 import api from '../../services/api';
+import { IResult } from '../../models/comic';
+
+const favorites: IResult[] = [
+  {
+    id: 82967,
+    digitalId: 0,
+    title: 'Marvel Previews (2017)',
+    issueNumber: 0,
+    variantDescription: '',
+    description: null,
+    modified: '2019-11-07T08:46:15-0500',
+    isbn: '',
+    upc: '75960608839302811',
+    diamondCode: '',
+    ean: '',
+    issn: '',
+    format: '',
+    pageCount: 112,
+    textObjects: [],
+    resourceURI: 'http://gateway.marvel.com/v1/public/comics/82967',
+    urls: [
+      {
+        type: 'detail',
+        url:
+          'http://marvel.com/comics/issue/82967/marvel_previews_2017?utm_campaign=apiRef&utm_source=10bab138cb8384426205710437215153',
+      },
+    ],
+    series: {
+      resourceURI: 'http://gateway.marvel.com/v1/public/series/23665',
+      name: 'Marvel Previews (2017 - Present)',
+    },
+    variants: [
+      {
+        resourceURI: 'http://gateway.marvel.com/v1/public/comics/82965',
+        name: 'Marvel Previews (2017)',
+      },
+      {
+        resourceURI: 'http://gateway.marvel.com/v1/public/comics/82970',
+        name: 'Marvel Previews (2017)',
+      },
+      {
+        resourceURI: 'http://gateway.marvel.com/v1/public/comics/82969',
+        name: 'Marvel Previews (2017)',
+      },
+      {
+        resourceURI: 'http://gateway.marvel.com/v1/public/comics/74697',
+        name: 'Marvel Previews (2017)',
+      },
+      {
+        resourceURI: 'http://gateway.marvel.com/v1/public/comics/72736',
+        name: 'Marvel Previews (2017)',
+      },
+      {
+        resourceURI: 'http://gateway.marvel.com/v1/public/comics/75668',
+        name: 'Marvel Previews (2017)',
+      },
+      {
+        resourceURI: 'http://gateway.marvel.com/v1/public/comics/65364',
+        name: 'Marvel Previews (2017)',
+      },
+      {
+        resourceURI: 'http://gateway.marvel.com/v1/public/comics/65158',
+        name: 'Marvel Previews (2017)',
+      },
+      {
+        resourceURI: 'http://gateway.marvel.com/v1/public/comics/65028',
+        name: 'Marvel Previews (2017)',
+      },
+      {
+        resourceURI: 'http://gateway.marvel.com/v1/public/comics/75662',
+        name: 'Marvel Previews (2017)',
+      },
+      {
+        resourceURI: 'http://gateway.marvel.com/v1/public/comics/74320',
+        name: 'Marvel Previews (2017)',
+      },
+      {
+        resourceURI: 'http://gateway.marvel.com/v1/public/comics/73776',
+        name: 'Marvel Previews (2017)',
+      },
+    ],
+    collections: [],
+    collectedIssues: [],
+    dates: [
+      {
+        type: 'onsaleDate',
+        date: '2099-10-30T00:00:00-0500',
+      },
+      {
+        type: 'focDate',
+        date: '2019-10-07T00:00:00-0400',
+      },
+    ],
+    prices: [
+      {
+        type: 'printPrice',
+        price: 0,
+      },
+    ],
+    thumbnail: {
+      path: 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available',
+      extension: 'jpg',
+    },
+    images: [],
+    creators: {
+      available: 1,
+      collectionURI:
+        'http://gateway.marvel.com/v1/public/comics/82967/creators',
+      items: [
+        {
+          resourceURI: 'http://gateway.marvel.com/v1/public/creators/10021',
+          name: 'Jim Nausedas',
+          role: 'editor',
+        },
+      ],
+      returned: 1,
+    },
+    characters: {
+      available: 0,
+      collectionURI:
+        'http://gateway.marvel.com/v1/public/comics/82967/characters',
+      items: [],
+      returned: 0,
+    },
+    stories: {
+      available: 2,
+      collectionURI: 'http://gateway.marvel.com/v1/public/comics/82967/stories',
+      items: [
+        {
+          resourceURI: 'http://gateway.marvel.com/v1/public/stories/183698',
+          name: 'cover from Marvel Previews (2017)',
+          type: 'cover',
+        },
+        {
+          resourceURI: 'http://gateway.marvel.com/v1/public/stories/183699',
+          name: 'story from Marvel Previews (2017)',
+          type: 'interiorStory',
+        },
+      ],
+      returned: 2,
+    },
+    events: {
+      available: 0,
+      collectionURI: 'http://gateway.marvel.com/v1/public/comics/82967/events',
+      items: [],
+      returned: 0,
+    },
+    dateFavorite: new Date('2020-01-01').getTime(),
+  },
+];
 
 const apiMock = new MockAdapater(api);
 
@@ -230,158 +381,6 @@ describe('Comic hook', () => {
   });
 
   it('should be able remove favorite to list', async () => {
-    const favorites = [
-      {
-        id: 82967,
-        digitalId: 0,
-        title: 'Marvel Previews (2017)',
-        issueNumber: 0,
-        variantDescription: '',
-        description: null,
-        modified: '2019-11-07T08:46:15-0500',
-        isbn: '',
-        upc: '75960608839302811',
-        diamondCode: '',
-        ean: '',
-        issn: '',
-        format: '',
-        pageCount: 112,
-        textObjects: [],
-        resourceURI: 'http://gateway.marvel.com/v1/public/comics/82967',
-        urls: [
-          {
-            type: 'detail',
-            url:
-              'http://marvel.com/comics/issue/82967/marvel_previews_2017?utm_campaign=apiRef&utm_source=10bab138cb8384426205710437215153',
-          },
-        ],
-        series: {
-          resourceURI: 'http://gateway.marvel.com/v1/public/series/23665',
-          name: 'Marvel Previews (2017 - Present)',
-        },
-        variants: [
-          {
-            resourceURI: 'http://gateway.marvel.com/v1/public/comics/82965',
-            name: 'Marvel Previews (2017)',
-          },
-          {
-            resourceURI: 'http://gateway.marvel.com/v1/public/comics/82970',
-            name: 'Marvel Previews (2017)',
-          },
-          {
-            resourceURI: 'http://gateway.marvel.com/v1/public/comics/82969',
-            name: 'Marvel Previews (2017)',
-          },
-          {
-            resourceURI: 'http://gateway.marvel.com/v1/public/comics/74697',
-            name: 'Marvel Previews (2017)',
-          },
-          {
-            resourceURI: 'http://gateway.marvel.com/v1/public/comics/72736',
-            name: 'Marvel Previews (2017)',
-          },
-          {
-            resourceURI: 'http://gateway.marvel.com/v1/public/comics/75668',
-            name: 'Marvel Previews (2017)',
-          },
-          {
-            resourceURI: 'http://gateway.marvel.com/v1/public/comics/65364',
-            name: 'Marvel Previews (2017)',
-          },
-          {
-            resourceURI: 'http://gateway.marvel.com/v1/public/comics/65158',
-            name: 'Marvel Previews (2017)',
-          },
-          {
-            resourceURI: 'http://gateway.marvel.com/v1/public/comics/65028',
-            name: 'Marvel Previews (2017)',
-          },
-          {
-            resourceURI: 'http://gateway.marvel.com/v1/public/comics/75662',
-            name: 'Marvel Previews (2017)',
-          },
-          {
-            resourceURI: 'http://gateway.marvel.com/v1/public/comics/74320',
-            name: 'Marvel Previews (2017)',
-          },
-          {
-            resourceURI: 'http://gateway.marvel.com/v1/public/comics/73776',
-            name: 'Marvel Previews (2017)',
-          },
-        ],
-        collections: [],
-        collectedIssues: [],
-        dates: [
-          {
-            type: 'onsaleDate',
-            date: '2099-10-30T00:00:00-0500',
-          },
-          {
-            type: 'focDate',
-            date: '2019-10-07T00:00:00-0400',
-          },
-        ],
-        prices: [
-          {
-            type: 'printPrice',
-            price: 0,
-          },
-        ],
-        thumbnail: {
-          path:
-            'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available',
-          extension: 'jpg',
-        },
-        images: [],
-        creators: {
-          available: 1,
-          collectionURI:
-            'http://gateway.marvel.com/v1/public/comics/82967/creators',
-          items: [
-            {
-              resourceURI: 'http://gateway.marvel.com/v1/public/creators/10021',
-              name: 'Jim Nausedas',
-              role: 'editor',
-            },
-          ],
-          returned: 1,
-        },
-        characters: {
-          available: 0,
-          collectionURI:
-            'http://gateway.marvel.com/v1/public/comics/82967/characters',
-          items: [],
-          returned: 0,
-        },
-        stories: {
-          available: 2,
-          collectionURI:
-            'http://gateway.marvel.com/v1/public/comics/82967/stories',
-          items: [
-            {
-              resourceURI: 'http://gateway.marvel.com/v1/public/stories/183698',
-              name: 'cover from Marvel Previews (2017)',
-              type: 'cover',
-            },
-            {
-              resourceURI: 'http://gateway.marvel.com/v1/public/stories/183699',
-              name: 'story from Marvel Previews (2017)',
-              type: 'interiorStory',
-            },
-          ],
-          returned: 2,
-        },
-        events: {
-          available: 0,
-          collectionURI:
-            'http://gateway.marvel.com/v1/public/comics/82967/events',
-          items: [],
-          returned: 0,
-        },
-        dateFavorite: new Date('2020-01-01').getTime(),
-      },
-    ];
-
     jest.spyOn(AsyncStorage, 'getItem').mockImplementation(key => {
       if (key === '@appMarvel:favorites') return JSON.stringify(favorites);
 
@@ -438,5 +437,38 @@ describe('Comic hook', () => {
     expect(getItemSpy).toBeCalled();
     expect(result.current.loading).toBeFalsy();
     expect(setItemSpy).toBeCalled();
+  });
+
+  it('should be able clear all favorites', async () => {
+    const { result } = renderHook(() => useFavorite(), {
+      wrapper: FavoriteProvider,
+    });
+
+    const alertSpy = jest.spyOn(Alert, 'alert');
+    const setItemSpy = jest.spyOn(AsyncStorage, 'setItem');
+
+    result.current.handleClearAll();
+
+    act(() => {
+      alertSpy.mock.calls[0][2][1].onPress();
+    });
+
+    expect(result.current.data).toEqual([]);
+  });
+
+  it('should be able cancel clear all favorites', () => {
+    const { result } = renderHook(() => useFavorite(), {
+      wrapper: FavoriteProvider,
+    });
+
+    const alertSpy = jest.spyOn(Alert, 'alert');
+
+    result.current.handleClearAll();
+
+    act(() => {
+      alertSpy.mock.calls[0][2][0].onPress();
+    });
+
+    expect(result.current.data).toEqual([]);
   });
 });
